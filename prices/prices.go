c1c2826bb557425879746db410a5155f44e6093e
sikehish <hisham0502@gmail.com>
2024-02-11 23:50:14 +0530

    Created a new struct and a constructor for it under prices package

--- prices/prices.go
+++ prices/prices.go
@@ -1,1 +1,14 @@
 package prices
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}
