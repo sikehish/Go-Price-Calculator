8773dc46931d6bdb27c2cd9dd446d525f7d2a639
sikehish <hisham0502@gmail.com>
2024-02-12 00:56:42 +0530

    Added more methods

--- main.go
+++ main.go
@@ -1,27 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/sikehish/Go-Price-Calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
+	// pricesData := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15} //0%,7%,10%,15%
 	//We apply different tax rates to each price
+	// result := make(map[float64][]float64, 3)
 
-	result := make(map[float64][]float64, 3)
-
-	for _, taxRate := range taxRates {
-		for _, price := range prices {
-			result[taxRate] = append(result[taxRate], price+(price*taxRate))
-		}
-	}
+	// for _, taxRate := range taxRates {
+	// 	for _, price := range pricesData {
+	// 		result[taxRate] = append(result[taxRate], price+(price*taxRate))
+	// 	}
+	// }
 	// //OR (More efficient way as we dont overwrite the slice in each iteration)
 	// for _, taxRate := range taxRates {
-	// 	taxIncludedPrices := make([]float64, 3)
-	// 	for priceIdx, price := range prices {
+	// 	taxIncludedpricesData := make([]float64, 3)
+	// 	for priceIdx, price := range pricesData {
 	// 		taxIncludedPrices[priceIdx] = price * (1 + taxRate)
 	// 	}
 	// 	result[taxRate] = taxIncludedPrices
 	// }
+	//OR
+	for _, taxRate := range taxRates {
+		pricesJob := prices.NewTaxIncludedPriceJob(taxRate)
+		pricesJob.Process()
+	}
 
-	fmt.Println(result)
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,14 +1,59 @@
 package prices
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
 
+// Constructor
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%v", price)] = price + price*job.TaxRate
+	}
+
+	fmt.Println(job.TaxRate, result)
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	pricesFile, err := os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(pricesFile)
+
+	var lines []string
+
+	for scanner.Scan() { //It'll iterate as long scanner.Scan() returns true.It returns false when the scan stops, either by reaching the end of the input or an error.
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err() //After Scan returns false, the Err method will return any error that occurred during scanning, except that if it was io.EOF, Err will return nil.
+
+	if err != nil {
+		fmt.Println("Reading the file content failed")
+		fmt.Println(err)
+		pricesFile.Close()
+		return
+	}
+
 }
